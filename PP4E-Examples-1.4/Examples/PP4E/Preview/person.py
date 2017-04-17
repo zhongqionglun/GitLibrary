7e913b56af82cd7a0665c04116a2df94746bd0fc
zhongqionglun <zhongqionglun@outlook.com>
2017-04-17 23:52:50 +0800

    add  the function:__str__

--- PP4E-Examples-1.4/Examples/PP4E/Preview/person.py
+++ PP4E-Examples-1.4/Examples/PP4E/Preview/person.py
@@ -8,12 +8,16 @@ class Person:
         return self.name.split()[-1]
     def giveRaise(self, percent):
         self.pay *= (1.0 + percent)
+    def __str__(self):
+        return ('<%s => %s: %s, %s, %s>'
+                %(self.__class__.__name__, self.name, self.age, self.job, self.pay))
 
 if __name__ == '__main__':
     bob = Person('Bob Smith', 42, 30000, 'software')
     sue = Person('Sue Jones', 45, 40000, 'hardware')
-    print(bob.name, sue.pay)
-
-    print(bob.lastName())
-    sue.giveRaise(.10)
-    print(sue.pay)
+    for i in (bob, sue):
+        print(i)
+    # print(bob.name, sue.pay)
+    # print(bob.lastName())
+    # sue.giveRaise(.10)
+    # print(sue.pay)
