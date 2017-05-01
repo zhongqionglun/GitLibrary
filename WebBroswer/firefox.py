8485f6d86a9d82db7fa34481837eb2a54178ba88
zhongqionglun <zhongqionglun@outlook.com>
2017-05-01 22:36:35 +0800

    resolve webdriver.Firefox:远程主机强制关闭了一个现有链接 add tests for web pages go & back

--- WebBroswer/firefox.py
+++ WebBroswer/firefox.py
@@ -1,7 +1,16 @@
+# -*- coding: utf-8 -*-
 from selenium import webdriver
+import time
 
+capabilities = webdriver.DesiredCapabilities().FIREFOX
+capabilities['marionette'] = False
 broswer = webdriver.Firefox()
 broswer.get("http://www.baidu.com")
+broswer.find_element_by_link_text(u'新闻').click()
+time.sleep(5)
+broswer.back()
+time.sleep(2)
 broswer.find_element_by_id("kw").send_keys("selenium")
 broswer.find_element_by_id("su").click()
+time.sleep(2)
 broswer.close()
