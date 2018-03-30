bc3db0ef3764f6cff0a8035bfcb9d4c72c365fd5
zhongqionglun <zhongqionglun@outlook.com>
2018-03-30 18:20:24 +0800

    100-000.py draw number

--- PythonLearning/Exercises100/100-001/100-001.py
+++ PythonLearning/Exercises100/100-001/100-001.py
@@ -1,2 +1,21 @@
 # -*-coding:UTF-8-*-
+import random, string
+
+
+class GenRcode():
+	
+	def genrcode():
+		chars = string.letters + string.digits
+		s = "".join(random.choice(chars) for i in range(10))
+		t = "".join(random.sample(chars, 10))
+		rcode = s + " ******** " + t
+		return rcode
+	genrcode = staticmethod(genrcode)
+
+if __name__ == "__main__":
+	rcode = []
+	for i in range(200):
+		rcode.append(GenRcode.genrcode())
+	for l in rcode:
+		print l + "\n"
 
