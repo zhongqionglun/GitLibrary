83a34ce1f076f6dceb8f03ef3624bcb069074d0c
zhongqionglun <zhongqionglun@outlook.com>
2018-11-25 01:00:46 +0800

    test

--- DWG/query-incoming-sms.py
+++ DWG/query-incoming-sms.py
@@ -1,16 +1,23 @@
 import requests
 
-url = "http://172.28.1.42/api/query_incoming_sms"
 
-headers = {
-    'Content-Type': "application/json",
-    'flag': "all",
-    'port': "0",
-    'Authorization': "Digest username=\"admin\", realm=\"Web Server\", nonce=\"0ef4291683b2e29974c078f47dc910f7\", uri=\"/api/query_incoming_sms\", algorithm=\"MD5\", qop=auth, nc=00000001, cnonce=\"UGO58uRk\", response=\"e8f69d461c98099f11988d05c7d2c73e\", opaque=\"5ccc069c403ebaf9f0171e9517f40e41\"",
-    'Cache-Control': "no-cache",
-    'Postman-Token': "386e6e56-c7a9-44e7-bf47-a4b93cb82a18"
+def query_incoming_sms(urladdr, reqheaders):
+    response = requests.request("GET", urladdr, reqheaders)
+    return response
+
+
+if __name__ == "__main__":
+    url = "http://172.28.1.42/api/query_incoming_sms"
+    headers = {
+        'Content-Type': "application/json",
+        'flag': "all",
+        'port': "0",
+        'Authorization': "Digest username=\"admin\", realm=\"Web Server\", nonce=\"0ef4291683b2e29974c078f47dc910f7\", uri=\"/api/query_incoming_sms\", algorithm=\"MD5\", qop=auth, nc=00000001, cnonce=\"UGO58uRk\", response=\"e8f69d461c98099f11988d05c7d2c73e\", opaque=\"5ccc069c403ebaf9f0171e9517f40e41\"",
+        'Cache-Control': "no-cache",
+        'Postman-Token': "386e6e56-c7a9-44e7-bf47-a4b93cb82a18"
     }
-
-response = requests.request("GET", url, headers=headers)
-
-print(response.text)
+    resp = query_incoming_sms(url, headers)
+    if not resp:
+        print("The response is empry!")
+    else:
+        print(resp.text)
--- DWG/query-result.py
+++ DWG/query-result.py
@@ -1,15 +1,23 @@
 import requests
 
-url = "http://172.28.1.42/api/query_sms_result"
 
-payload = "{port\":[0,0],\"number\":\"10086\",\"user_id\":1}"
-headers = {
-    'Content-Type': "application/json",
-    'Authorization': "Digest username=\"admin\", realm=\"Web Server\", nonce=\"ad86841c71da9a3f88679ee01d2f1ed2\", uri=\"/api/query_sms_result\", algorithm=\"MD5\", qop=auth, nc=00000001, cnonce=\"4Efsy2xG\", response=\"0674bea4c2ef6acaadd48d4f5bdf9a3b\", opaque=\"5ccc069c403ebaf9f0171e9517f40e41\"",
-    'Cache-Control': "no-cache",
-    'Postman-Token': "7390ebd5-13e2-40c4-9ffb-9c0e534f3580"
+def query_result(urladdr, jsondata, reqheaders):
+    response = requests.request("POST", urladdr, jsondata, reqheaders)
+    return response
+
+
+if __name__ == "__main__":
+    url = "http://172.28.1.42/api/query_sms_result"
+    payload = "{port\":[0,0],\"number\":\"10086\",\"user_id\":1}"
+    headers = {
+        'Content-Type': "application/json",
+        'Authorization': "Digest username=\"admin\", realm=\"Web Server\", nonce=\"ad86841c71da9a3f88679ee01d2f1ed2\", uri=\"/api/query_sms_result\", algorithm=\"MD5\", qop=auth, nc=00000001, cnonce=\"4Efsy2xG\", response=\"0674bea4c2ef6acaadd48d4f5bdf9a3b\", opaque=\"5ccc069c403ebaf9f0171e9517f40e41\"",
+        'Cache-Control': "no-cache",
+        'Postman-Token': "7390ebd5-13e2-40c4-9ffb-9c0e534f3580"
     }
-
-response = requests.request("POST", url, data=payload, headers=headers)
-
-print(response.text)
+    resp = query_result(url, payload, headers)
+    if not resp:
+        print("The response is empty!")
+    else:
+        print(resp.text)
+        
