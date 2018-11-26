9a2df66455ab9abcd38aed6cd7dd96148b16599d
zhongqionglun <zhongqionglun@outlook.com>
2018-11-26 12:51:49 +0800

    test

--- DWG/query-sms-in-queue.py
+++ DWG/query-sms-in-queue.py
@@ -1,14 +1,19 @@
 import requests
 
-url = "http://172.28.1.42/api/query_sms_in_queue"
+def query_sms_in_queue(urladdr, reqheaders):
+    response = requests.request("GET", urladdr, reqheaders)
+    return  response
 
-headers = {
-    'Content-Type': "application/json",
-    'Authorization': "Digest username=\"admin\", realm=\"Web Server\", nonce=\"85498d5c680fff2fb3443132b22f2479\", uri=\"/api/query_sms_in_queue\", algorithm=\"MD5\", qop=auth, nc=00000001, cnonce=\"l3dEgF2E\", response=\"acdc7f09eb0ad9cecac54b9283f946e2\", opaque=\"5ccc069c403ebaf9f0171e9517f40e41\"",
-    'Cache-Control': "no-cache",
-    'Postman-Token': "4212f586-6c37-4462-b391-c623d9f785ee"
+if __name__ == "__main__":
+    url = "http://172.28.1.42/api/query_sms_in_queue"
+    headers = {
+        'Content-Type': "application/json",
+        'Authorization': "Digest username=\"admin\", realm=\"Web Server\", nonce=\"85498d5c680fff2fb3443132b22f2479\", uri=\"/api/query_sms_in_queue\", algorithm=\"MD5\", qop=auth, nc=00000001, cnonce=\"l3dEgF2E\", response=\"acdc7f09eb0ad9cecac54b9283f946e2\", opaque=\"5ccc069c403ebaf9f0171e9517f40e41\"",
+        'Cache-Control': "no-cache",
+        'Postman-Token': "4212f586-6c37-4462-b391-c623d9f785ee"
     }
-
-response = requests.request("GET", url, headers=headers)
-
-print(response.text)
+    resp = query_sms_in_queue(url, headers)
+    if not resp:
+        print("The response is empty!")
+    else:
+        print(resp.text)
--- DWG/set_trunk.py
+++ DWG/set_trunk.py
@@ -1,15 +1,22 @@
 import requests
 
-url = "http://172.28.1.42/api/set_config"
 
-payload = "{ \"sip_trunk\":[{\"index\":31,\"enable\":\"on\",\"domain\":\"172.28.1.46\",\"port\":5060,\"description\":\"benji\",\"check_network_status\":\"on\"}] }"
-headers = {
-    'Content-Type': "application/json",
-    'Authorization': "Digest username=\"admin\", realm=\"Web Server\", nonce=\"2b4c82af05bbbd60d2abff09f2461a63\", uri=\"/api/set_config\", algorithm=\"MD5\", qop=auth, nc=00000001, cnonce=\"X7F4hPdi\", response=\"8ab017746bf7e002c50bcbc211b07607\", opaque=\"5ccc069c403ebaf9f0171e9517f40e41\"",
-    'cache-control': "no-cache",
-    'Postman-Token': "997ab256-49e1-47d0-925b-10b79ae76b13"
+def set_trunk(urladdr, jsondata, reqheaders):
+    response = requests.request("POST", urladdr, jsondata, reqheaders)
+    return response
+
+if __name__=="__main__":
+    url = "http://172.28.1.42/api/set_config"
+
+    payload = "{ \"sip_trunk\":[{\"index\":31,\"enable\":\"on\",\"domain\":\"172.28.1.46\",\"port\":5060,\"description\":\"benji\",\"check_network_status\":\"on\"}] }"
+    headers = {
+        'Content-Type': "application/json",
+        'Authorization': "Digest username=\"admin\", realm=\"Web Server\", nonce=\"2b4c82af05bbbd60d2abff09f2461a63\", uri=\"/api/set_config\", algorithm=\"MD5\", qop=auth, nc=00000001, cnonce=\"X7F4hPdi\", response=\"8ab017746bf7e002c50bcbc211b07607\", opaque=\"5ccc069c403ebaf9f0171e9517f40e41\"",
+        'cache-control': "no-cache",
+        'Postman-Token': "997ab256-49e1-47d0-925b-10b79ae76b13"
     }
-
-response = requests.request("POST", url, data=payload, headers=headers)
-
-print(response.text)
+    resp = set_trunk(url, payload, headers)
+    if not resp:
+        print("The response is empty!")
+    else:
+        print(resp.text)
