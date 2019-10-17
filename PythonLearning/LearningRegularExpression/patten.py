49203078216853f5e496894c8632535c902c6bba
zhongqionglun <zhongqionglun@outlook.com>
2019-10-17 18:30:55 +0800

    2019/3/22

---
+++ PythonLearning/LearningRegularExpression/patten.py
@@ -0,0 +1,32 @@
+# -*-coding:UTF-8-*-
+import re
+
+"""
+auth:zhongqionglun
+time:
+description:该文档显示通过函数模板来动态生产正则表达式匹配规则
+"""
+
+patterns = (('[sxz]$', '$', 'es'),
+            ('[^aeioudgkprt]h$', '$', 'es'),
+            ('(qu|[^aeiou])y$', 'y$', 'ies'),
+            ('$', '$', 's')
+            )
+
+
+def build_match_and_apply_fuctons(pattern, search, replace):
+	def matches_rule(word):
+		return re.search(pattern, word)
+	
+	def apply_rule(word):
+		return re.sub(search, replace, word)
+	
+	return (matches_rule, apply_rule)
+
+rules = [build_match_and_apply_fuctons(pattern, search, replace) for (pattern, search, replace) in patterns]
+
+def plural(noun):
+	for matches_rule, aply_rule in rules:
+		if matches_rule(noun):
+			return apply_rule(noun)
+
